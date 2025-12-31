3914b726f4cfc7080410c8709f7de5d766d376e4
timonpolster <tpolster@student.tgm.ac.at>
2025-12-31 13:00:06 +0100

    config.py erstellt um mittels Pydantic Settings eine sicherung aller benätigten Daten zu gewährleisten

---
+++ app/core/config.py
@@ -0,0 +1,37 @@
+from typing import Literal
+from pydantic import computed_field, PostgresDsn
+from pydantic_core import MultiHostUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_ignore_empty=True,
+        extra="ignore",
+        case_sensitive=True
+    )
+
+    API_V1_STR: str = "/api/v1"
+    PROJECT_NAME: str = "WeMood Backend"
+    ENVIRONMENT: Literal["dev", "test", "prod"] = "dev"
+
+    POSTGRES_SERVER: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str = "wemood_db"
+
+    MISTRAL_API_KEY: str
+
+    @computed_field
+    def SQLALCHEMY_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+asyncpg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
+
+settings = Settings()
