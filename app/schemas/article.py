664e3e8b169c0ef618ffcb9d7cd8059079a12e7a
timonpolster <tpolster@student.tgm.ac.at>
2025-12-31 12:11:36 +0100

    Regeln für die Inhalte definiert, koennen spaeter noch veraendert werden falls notwendig

---
+++ app/schemas/article.py
@@ -0,0 +1,12 @@
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel, Field, ConfigDict
+
+class ArticleCreate(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    title: str = Field(..., min_length=5, max_length=255)
+    content: str = Field(..., min_length=50)
+    source: Optional[str] = Field(None, max_length=255)
+    url: Optional[str] = Field(None)
+    publication_date: Optional[date] = Field(None)
